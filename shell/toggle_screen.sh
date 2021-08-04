21e48267c6132945741faca43bfefdc4b09786f2
Mila Page <versusfacit@users.noreply.github.com>
2021-08-03 19:46:33 -0700

    Add script to manage screen settings.

---
+++ shell/toggle_screen.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -o errexit
+
+declare -r BL_DIR='/sys/class/backlight'
+declare -r CHIP_DIR='intel_backlight'
+declare -r COOL_WHITE=3500
+declare -r MATCH_FLAME=1500
+
+# adjust brightness
+modulate_brightness() {
+
+  local max_brightness_value
+  local minimum_brightness_value
+
+  local -r factor="${*}"
+  declare -r filename="${BL_DIR}/${CHIP_DIR}/max_brightness"
+
+  max_brightness_value="$(head -n 1 "${filename}")"
+  minimum_brightness_value="$(bc <<<"${max_brightness_value} * ${factor}/1" )"
+
+  sudo su -c "echo ${minimum_brightness_value} \
+    > ${BL_DIR}/${CHIP_DIR}/brightness"
+}
+
+redshift_now() {
+  redshift -P -O "$*" > /dev/null
+}
+
+printf "Choose a command: \n %s\n %s\n %s\n %s\n %s\n" \
+  "1. Use match flame" \
+  "2. Use cool white" \
+  "3. Set to darkroom brightness" \
+  "4. Set to usual daytime brightness" \
+  "4. Set to brighter daytime brightness"
+
+while read -s -r -n1 user_input; do
+  case "${user_input}" in
+    1) redshift_now "${MATCH_FLAME}" ;;
+    2) redshift_now "${COOL_WHITE}" ;;
+    3) modulate_brightness 0.02;;
+    4) modulate_brightness 0.07;;
+    5) modulate_brightness 0.15;;
+    *) printf "Exiting script.\n" ; exit 0
+  esac
+done
+
